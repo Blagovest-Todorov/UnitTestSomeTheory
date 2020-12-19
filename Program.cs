2fa48b9f2b9b0f3aa95973e8bf01f30ac790c9c3
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-19 19:16:59 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GallioAutomationPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Gallio Automation Platform --platforms that supports diff tests, writen on different Frameworks!
+
+            // int Sum(int a, int b) - > returns sum of a and b
+            //public void Sum_AddOneAndTwo()
+            //{
+            //    int result = Sum_AddOneAndTwo(1, 2);
+            //    Assert.AreEqual(4, result, "Bad Sum");
+            //}
+            // a test should provide that there is sth wrong with poduction code, not witht the unsit test code !
+            // The idea of the unit test it to prove that our code is working , or not working,
+            // to show how the code works under differenct cases!
+            // Assert message is Important ! , Good message helps to track Bugs and understatnd unit tests more easily !
+            // the message shows if the test failed, if the expected result is different than the  actual result !
+            // the message to show what we tested , what happend
+            // unit Test they check concrete things !
+            // avoid using multiple asserts in a single test case !
+            // one test - > one assert !
+            // writing unitTests is hard !
+        }
+    }
+}
